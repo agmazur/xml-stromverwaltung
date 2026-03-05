268539f580bc8624cfd0db4da46d6f12312f6871
Andrii Mazur <agmazur888@gmail.com>
2026-03-05 16:54:14 +0100

    added server - client communication for sending messeges, finaly

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,45 +1,24 @@
-console.log("Comment script loaded");
-window.onload = function() {
-    const myButton = document.getElementById('add_coment');
-    myButton.addEventListener('click', function() {
-    });
-    } 
-;
-
-
-async function addComment() {
-    const newComment = {
-        name: "User Name", // You'd get this from an input field
-        text: "This is a new comment!",
-        date: new Date().toLocaleDateString()
-    };
-
-    const response = await fetch('/add-comment', {
+async function sent_coment_server() {
+    
+    const response = await fetch('/add_coment', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newComment)
+        body: JSON.stringify({ message: "cool mesege",name:"andrii",date:new Date().toISOString().split('T')[0],})
     });
 
-    if (response.ok) {
-        location.reload(); // Refresh to see the new comment via your XSLT
-    }
+    const data = await response.json();
+    console.log("Response from server:", data.status);
+    reload_iframe()
 }
 
-
-
-async function sendCommentToServer() {
-    const data = {
-        name: "Andrii Mazur",
-        text: "This will actually save to the file!",
-        date: "09-02-26"
-    };
-
-    // We "POST" the data to a specific URL on our Node server
-    await fetch('/save-comment', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    });
-    
-    alert("Saved to server!");
+window.onload = function() {
+    const myButton = document.getElementById('add_coment');
+    myButton.addEventListener('click',sent_coment_server );
+    } 
+;
+function reload_iframe() {
+    const iframe = document.getElementById(iframeId);
+    if (iframe) {
+        iframe.contentWindow.location.reload();
+    }
 }
